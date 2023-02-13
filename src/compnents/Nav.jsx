d74c1e65eeedaa236c3f82ff89b2e16fea9e0b86
weijun1923 <asd101250101@gmail.com>
2023-02-13 17:51:28 +0800

    註冊

--- src/compnents/Nav.jsx
+++ src/compnents/Nav.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 // import { ReactComponent as Logo } from '../assets/logo.svg'
-import Logo from "../assets/logo.svg";
+import Logo from "../assets/logoEnglish.svg";
 import { FaShoppingCart, FaSearch, FaUser, FaAngleLeft, FaDoorOpen, FaDoorClosed } from "react-icons/fa"; //購物車
 
 function Nav() {
@@ -31,19 +31,19 @@ function Nav() {
 
   return (
     // nav
-    <div className='nav flex justify-between items-center shadow-lg p-2 w-screen relative'>
+    <div className='nav flex justify-between items-center shadow-lg p-2 w-screen relative '>
       {navChange && (
         // logo
-        <Link to='/price-watch-observer-client/'>
-          <img src={Logo} alt="價格觀察網logo" className=' w-16 h-16 lg:w-20 lg:h-20 ml-5 mr-10' id='logo'/>
+        <Link to='/price-watch-observer-client/' className='p-0'>
+          <img src={Logo} alt="價格觀察網logo" className=' w-32 h-16 lg:w-[200px] lg:h-26 ml-2 lg:mr-20 ' id='logo'/>
         </Link>
       )}
 
       {/* searchBar */}
-      {!navChange ? <FaAngleLeft size={30} onClick={() => setNavChange(!navChange)} /> : <FaAngleLeft className='hidden' />}
-      <form action="" className={!navChange ? 'flex bg-gray-200 p-2  justify-center items-center rounded-full w-full' : 'hidden lg:flex bg-gray-200 p-2 flex-grow justify-center items-center rounded-full w-full'}>
-        <FaSearch size={30} />
-        <input type="text" className=' bg-inherit  text-2xl pl-3 outline-none w-full' placeholder='搜尋商品' onChange={handleSearch} value={input} />
+      {!navChange ? <FaAngleLeft size={30} onClick={() => setNavChange(!navChange)} className='mx-2'/> : <FaAngleLeft className='hidden' />}
+      <form action="" className={!navChange ? 'flex bg-gray-200 p-2  justify-center items-center rounded-full w-full' : 'hidden lg:flex bg-gray-200 p-2  justify-center items-center rounded-full w-2/4'}>
+        <FaSearch size={25} />
+        <input type="text" className=' bg-inherit  lg:text-2xl text-xl  pl-3 outline-none w-full' placeholder='搜尋商品' onChange={handleSearch} value={input} />
       </form>
       {navChange && (
         // icons
@@ -55,19 +55,19 @@ function Nav() {
                 <FaSearch className=' w-6 h-6 ' />
               </Link>
             </li>
-            <li className=' bg-gray-200 p-2 rounded-full mx-1 hover:bg-gray-400'>
+            <li className=' bg-gray-200 lg:p-3 p-2 rounded-full mx-1 hover:bg-gray-400'>
               {/* shopcart */}
               <Link to='/price-watch-observer-client/cart'>
-                <FaShoppingCart className=' w-6 h-6 lg:w-7 lg:h-7' />
+                <FaShoppingCart className=' w-6 h-6 lg:w-10 lg:h-10' />
               </Link>
             </li>
-            <li className=' bg-gray-200 p-2 rounded-full mx-1 mr-5 hover:bg-gray-400 ' onClick={() => setAuthToggle(!authToggle)} id='auth'>
+            <li className=' bg-gray-200 lg:p-3 p-2  rounded-full mx-1 mr-5 hover:bg-gray-400 ' onClick={() => setAuthToggle(!authToggle)} id='auth'>
               {/* auth */}
-              <FaUser className=' w-6 h-6 lg:w-7 lg:h-7 ' />
-              <ul  className={authToggle ? ' z-[1000] absolute right-0 w-2/3 lg:w-2/6 bg-white top-[90px] lg:top-[100px] h-screen  shadow-lg flex flex-col justify-start items-start ease-in-out duration-300' : 'z-[1000] absolute  right-0  bg-white border-l border-gray-400 flex flex-col justify-start items-start translate-x-full'}>
+              <FaUser className=' w-6 h-6 lg:h-10 lg:w-10 ' />
+              <ul  className={authToggle ? ' z-[1000] absolute right-0 w-2/3 lg:w-2/6 bg-white top-[90px] lg:top-[100px] h-screen  shadow-lg flex flex-col justify-start items-start ease-in-out duration-300' : ' fixed left-[-100%]'}>
                 <Link to='/price-watch-observer-client/personal' className='flex justify-start items-center my-5 w-full hover:bg-gray-400 p-2 pl-5 hover:rounded-sm'>
                   <li className=' bg-gray-200 rounded-full p-2 mr-3'>
-                    <FaUser className=' w-6 h-6  lg:w-7 lg:h-7' />
+                    <FaUser className=' w-6 h-6  lg:h-10 lg:w-10' />
                   </li>
                   <span>個人資料</span>
                 </Link>
--- src/compnents/Register.jsx
+++ src/compnents/Register.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import Logo from "../assets/logo.svg";
+import { Link } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
+import { BsFacebook } from 'react-icons/bs';
+import Line from '../assets/LINE.png';
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className='register bg-gray-100 w-full text-black pt-5 font-bold '>
+      <div className='flex flex-col justify-center items-center'>
+        <img src={Logo} alt="價格觀察網logo" className='w-[50px]' />
+        <h1 className='text-2xl'>註冊會員</h1>
+        <h4 className='text-md'>或者 <Link to='/price-watch-observer-client/login' className=' text-blue-500 text-md'>有會員了嗎? 點此登入</Link></h4>
+      </div>
+      <div className='flex flex-col justify-center items-center bg-white w-full pt-5 mt-3 shadow-xl'>
+        <form action="" className='flex flex-col w-4/5'>
+          <label htmlFor="username">使用者名稱 :</label>
+          <input type="text" name='username' className=' border border-gray-400 rounded-md pl-2' />
+          <label htmlFor="email">電子信箱 :</label>
+          <input type="email" name='email'  className=' border border-gray-400 rounded-md pl-2' />
+          <label htmlFor="phone">手機 :</label>
+          <input type="tel" name='phone' className=' border border-gray-400 rounded-md pl-2' />
+          <label htmlFor="password">密碼 :</label>
+          <input type="password" name='password' className=' border border-gray-400 rounded-md pl-2' />
+          <button className=' bg-red-500 text-white rounded-md mt-5 p-1'>註冊</button>
+          <h4 className='text-center text-gray-400 mt-3 before:content-[""] border-b border-gray-400 '>第三方註冊</h4>
+          <div className='flex justify-between items-center mb-7 mt-3'>
+            <button className='border border-gray-400 rounded-md px-8 py-2'><BsFacebook className='w-6 h-6 text-blue-600'/></button>
+            <button className='border border-gray-400 rounded-md px-8 py-2'><FcGoogle className='w-6 h-6'/></button>
+            <button className='border border-gray-400 rounded-md px-8 py-2'><img src={Line} alt="" className='w-6 h-6' /></button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
