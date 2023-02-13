ec996127c581988d9d4e83386e338a54bd5f25b6
weijun1923 <asd101250101@gmail.com>
2023-02-13 12:54:39 +0800

    nav fix

--- src/compnents/Nav.jsx
+++ src/compnents/Nav.jsx
@@ -1,31 +1,41 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 // import { ReactComponent as Logo } from '../assets/logo.svg'
 import Logo from "../assets/logo.svg";
 import { FaShoppingCart, FaSearch, FaUser, FaAngleLeft, FaDoorOpen, FaDoorClosed } from "react-icons/fa"; //購物車
 
 function Nav() {
+  const [authToggle, setAuthToggle] = useState(false);
   const [navChange, setNavChange] = useState(true);
   const [input, setInput] = useState('');
-  const [toggle, setToggle] = useState(false);
   const navigate = useNavigate(); //轉址
+
   const handleSearch = (e) => {
     setInput(e.value);
     navigate('/price-watch-observer-client/search');
     console.log(e.target.value);
   }
 
-  const handleToggle = (e) => {
-    e.stopPropagation();
-    setToggle(!toggle);
-  }
+  useEffect(() => {
+    const handleClick = (event) => {
+      if (!event.target.closest('#auth')) {
+        setAuthToggle(false);
+      }
+    };
+
+    document.addEventListener('click', handleClick);
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  }, []);
+
   return (
     // nav
     <div className='nav flex justify-between items-center shadow-lg p-2 w-screen relative'>
       {navChange && (
         // logo
         <Link to='/price-watch-observer-client/'>
-          <img src={Logo} alt="價格觀察網logo" className=' w-16 h-16 lg:w-20 lg:h-20 ml-5 mr-10' />
+          <img src={Logo} alt="價格觀察網logo" className=' w-16 h-16 lg:w-20 lg:h-20 ml-5 mr-10' id='logo'/>
         </Link>
       )}
 
@@ -42,45 +52,45 @@ function Nav() {
             <li className=' bg-gray-200 p-2 rounded-full mx-1 lg:hidden hover:bg-gray-400' onClick={() => setNavChange(!navChange)}>
               {/* search */}
               <Link>
-                <FaSearch size={30} />
+                <FaSearch className=' w-6 h-6 ' />
               </Link>
             </li>
             <li className=' bg-gray-200 p-2 rounded-full mx-1 hover:bg-gray-400'>
               {/* shopcart */}
               <Link to='/price-watch-observer-client/cart'>
-                <FaShoppingCart size={30} />
+                <FaShoppingCart className=' w-6 h-6 lg:w-7 lg:h-7' />
               </Link>
             </li>
-            <li className=' bg-gray-200 p-2 rounded-full mx-1 mr-5 hover:bg-gray-400 '>
+            <li className=' bg-gray-200 p-2 rounded-full mx-1 mr-5 hover:bg-gray-400 ' onClick={() => setAuthToggle(!authToggle)} id='auth'>
               {/* auth */}
-              <FaUser size={30} onClick={handleToggle}/>
-              {toggle && (
-                <ul className=' z-[1000] absolute right-0 w-1/2 bg-white top-[80px] h-screen border-l border-gray-400 flex flex-col justify-start items-start '>
-                  <Link to='/price-watch-observer-client/personal' className='flex justify-start items-center my-5 w-full hover:bg-gray-400 p-2 pl-5 hover:rounded-md'>
-                    <li className=' bg-gray-200 rounded-full p-2 mr-3'>
-                      <FaUser className=' w-6 h-6  lg:w-10 lg:h-10' />
-                    </li>
-                    <span>個人資料</span>
-                  </Link>
-                  <Link to='/price-watch-observer-client/login' className='flex justify-start items-center my-5 w-full hover:bg-gray-400 p-2 pl-5 hover:rounded-md'>
-                    <li className=' bg-gray-200 rounded-full p-2 mr-3'>
-                      <FaDoorOpen className=' w-6 h-6 lg:w-10 lg:h-10' />
-                    </li>
-                    <span>登入</span>
-                  </Link>
-                  <Link className='flex justify-start items-center my-5 w-full hover:bg-gray-400 p-2 pl-5 hover:rounded-md'>
-                    <li className=' bg-gray-200 rounded-full p-2 mr-3'>
-                      <FaDoorClosed className=' w-6 h-6 lg:w-10 lg:h-10' />
-                    </li>
-                    <span>登出</span>
-                  </Link>
-                </ul>
-              )}
+              <FaUser className=' w-6 h-6 lg:w-7 lg:h-7 ' />
+              <ul  className={authToggle ? ' z-[1000] absolute right-0 w-2/3 lg:w-2/6 bg-white top-[90px] lg:top-[100px] h-screen  shadow-lg flex flex-col justify-start items-start ease-in-out duration-300' : 'z-[1000] absolute  right-0  bg-white border-l border-gray-400 flex flex-col justify-start items-start translate-x-full'}>
+                <Link to='/price-watch-observer-client/personal' className='flex justify-start items-center my-5 w-full hover:bg-gray-400 p-2 pl-5 hover:rounded-sm'>
+                  <li className=' bg-gray-200 rounded-full p-2 mr-3'>
+                    <FaUser className=' w-6 h-6  lg:w-7 lg:h-7' />
+                  </li>
+                  <span>個人資料</span>
+                </Link>
+                <Link to='/price-watch-observer-client/login' className='flex justify-start items-center my-5 w-full hover:bg-gray-400 p-2 pl-5 hover:rounded-sm'>
+                  <li className=' bg-gray-200 rounded-full p-2 mr-3'>
+                    <FaDoorOpen className=' w-6 h-6 lg:w-7 lg:h-7' />
+                  </li>
+                  <span>登入</span>
+                </Link>
+                <Link className='flex justify-start items-center my-5 w-full hover:bg-gray-400 p-2 pl-5 hover:rounded-sm'>
+                  <li className=' bg-gray-200 rounded-full p-2 mr-3'>
+                    <FaDoorClosed className=' w-6 h-6 lg:w-7 lg:h-7' />
+                  </li>
+                  <span>登出</span>
+                </Link>
+              </ul>
+
             </li>
           </ul>
         </div>
       )}
     </div>
+
   )
 }
 
