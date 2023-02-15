fae6319d8d56bef3b4cc08cac1744b2c5963bffe
weijun1923 <asd101250101@gmail.com>
2023-02-15 23:30:37 +0800

    login update hood

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,10 @@ import Cart from "./compnents/Cart";
 import Login from "./compnents/Login";
 import Register from "./compnents/Register";
 import Profile from "./compnents/Profile";
+import AuthService from "./services/auth.service";
 
 function App() {
+  let [currentUser, setCurrentUser] = useState(AuthService.getCurrentUser());
   return (
     <div>
       <Nav/>
@@ -17,9 +19,9 @@ function App() {
         <Route path="/price-watch-observer-client/" element={<Home />} />
         <Route path="/price-watch-observer-client/search" element={<Search />} />
         <Route path="/price-watch-observer-client/cart" element={<Cart />} />
-        <Route path="/price-watch-observer-client/login" element={<Login />} />
+        <Route path="/price-watch-observer-client/login" element={<Login currentUser={currentUser} setCurrentUser={setCurrentUser}/>} />
         <Route path="/price-watch-observer-client/register" element={<Register />} />
-        <Route path="/price-watch-observer-client/Profile" element={<Profile />} />
+        <Route path="/price-watch-observer-client/Profile" element={<Profile currentUser={currentUser} setCurrentUser={setCurrentUser}/>} />
         
       </Routes>
       <Footer/>
